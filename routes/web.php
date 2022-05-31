e19c9846aa3f3aa24ff036478b4544f53dc9090a
omarelnakib <oselnakib@hotmail.com>
2022-05-31 20:03:39 +0200

    add auth service

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/user/{user}', [UserController::class,'index'])->name('user.show');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
